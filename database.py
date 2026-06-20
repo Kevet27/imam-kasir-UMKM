d863d3150a09dd273aaf2ea88872082856c0a0f9
Kevet27 <kevetjunior@gmail.com>
2026-06-21 00:09:17 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,7 @@ DB_NAME = "kasir.db"
 def get_connection():
     conn = sqlite3.connect(
         DB_NAME,
-        check_same_thread=False,
-        timeout=30
+        timeout=30,
+        check_same_thread=False
     )
-    conn.row_factory = sqlite3.Row
-    return conn
+    conn.row_factory
