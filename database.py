b312cf908613d8aa7cf1533419cacab06fe86171
Kevet27 <kevetjunior@gmail.com>
2026-06-20 23:56:59 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,55 +3,54 @@ import sqlite3
 DB_NAME = "kasir.db"
 
 def get_connection():
-    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT UNIQUE,
-password TEXT,
-role TEXT DEFAULT 'user'
-)
-""")
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS products(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-nama_barang TEXT,
-harga INTEGER,
-stok INTEGER
-)
-""")
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS transactions(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-total INTEGER
-)
-""")
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS transaction_detail(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-transaction_id INTEGER,
-product_id INTEGER,
-qty INTEGER,
-subtotal INTEGER
-)
-""")
-
-conn.commit()
-
-try:
-    cur.execute(
-        "INSERT INTO users(username,password,role) VALUES(?,?,?)",
-        ("admin","admin123","admin")
+    conn = sqlite3.connect(
+        DB_NAME,
+        check_same_thread=False,
+        timeout=30
     )
+
+    conn.row_factory = sqlite3.Row
+
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT,
+        role TEXT DEFAULT 'user'
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        nama_barang TEXT,
+        harga INTEGER,
+        stok INTEGER
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS transactions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        total INTEGER
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS transaction_detail(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        transaction_id INTEGER,
+        product_id INTEGER,
+        qty INTEGER,
+        subtotal INTEGER
+    )
+    """)
+
     conn.commit()
-except:
-    pass
+
+    return conn
